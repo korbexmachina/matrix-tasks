27f06186392b7ac7ba045cc4150151354a147d8e
Korben Tompkin <contact@korbexmachina.com>
2024-01-02 23:11:56 -0800

    working on TUI

--- main.go
+++ main.go
@@ -5,13 +5,15 @@ import (
 	"os"
 	// "log"
 
-	// utils "github.com/korbexmachina/matrix-tasks/utils"
+	utils "github.com/korbexmachina/matrix-tasks/utils"
 	tui "github.com/korbexmachina/matrix-tasks/tui"
 	tea "github.com/charmbracelet/bubbletea"
 )
 
 func main() {
-	p := tea.NewProgram(tui.Init())
+	utils.InitDB("test.db")
+	model := tui.InitialModel("test.db")
+	p := tea.NewProgram(model)
 	if _, err := p.Run(); err != nil {
 		fmt.Printf("Alas, there's been an error: %v", err)
 		os.Exit(1)
--- tui/ui.go
+++ tui/ui.go
@@ -2,73 +2,95 @@ package tui
 
 import (
 	"fmt"
-	"os"
+	// "os"
 
 	utils "github.com/korbexmachina/matrix-tasks/utils"
 	tea "github.com/charmbracelet/bubbletea"
 )
 
 type model struct {
-	tasks []utils.Task // tasks to display
+        UrgentImportant []utils.Task // tasks to display
+        UrgentNotImportant []utils.Task // tasks to display
+        NotUrgentImportant []utils.Task // tasks to display
+        NotUrgentNotImportant []utils.Task // tasks to display
 	cursor int // which task is pointed to
 	selected map[int]struct{} // which tasks are selected
 }
 
 // Initializes the model
-func initialModel(path string) model {
-	return model{
-		tasks: utils.ReadTasks(path),
-		cursor: 0,
-		selected: make(map[int]struct{}),
-	}
-}
+func InitialModel(path string) model {
+    urgentImportant, err := utils.GetBucketTasks("UrgentImportant")
+    if err != nil {
+        panic(err)
+    }
+    urgentNotImportant, err := utils.GetBucketTasks("UrgentNotImportant")
+    if err != nil {
+        panic(err)
+    }
+    notUrgentImportant , err := utils.GetBucketTasks("NotUrgentImportant")
+    if err != nil {
+        panic(err)
+    }
+    notUrgentNotImportant, err := utils.GetBucketTasks("NotUrgentNotImportant")
+    if err != nil {
+        panic(err)
+    }
+    return model{
+        UrgentImportant: urgentImportant,
+        UrgentNotImportant: urgentNotImportant,
+        NotUrgentImportant: notUrgentImportant,
+        NotUrgentNotImportant: notUrgentNotImportant,
+        cursor: 0,
+        selected: make(map[int]struct{}),
+        }
+    }
 
-// Init initializes the program
-func (m model) Init() tea.Cmd {
-	// Just return `nil`, which means "no I/O right now, please."
-	return nil
-}
+        // Init initializes the program
+        func (m model) Init() tea.Cmd {
+        // Just return `nil`, which means "no I/O right now, please."
+        return nil
+    }
 
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+        func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
     switch msg := msg.(type) {
 
     // Is it a key press?
     case tea.KeyMsg:
 
-        // Cool, what was the actual key pressed?
-        switch msg.String() {
+    // Cool, what was the actual key pressed?
+    switch msg.String() {
 
-        // These keys should exit the program.
-        case "ctrl+c", "q":
-            return m, tea.Quit
+    // These keys should exit the program.
+    case "ctrl+c", "q":
+    return m, tea.Quit
 
-        // The "up" and "k" keys move the cursor up
-        case "up", "k":
-            if m.cursor > 0 {
-                m.cursor--
-            }
+    // The "up" and "k" keys move the cursor up
+    case "up", "k":
+    if m.cursor > 0 {
+    m.cursor--
+}
 
-        // The "down" and "j" keys move the cursor down
-        case "down", "j":
-            if m.cursor < len(m.tasks)-1 {
-                m.cursor++
-            }
+// The "down" and "j" keys move the cursor down
+case "down", "j":
+if m.cursor < len(m.tasks)-1 {
+    m.cursor++
+}
 
-        // The "enter" key and the spacebar (a literal space) toggle
-        // the selected state for the item that the cursor is pointing at.
-        case "enter", " ":
-            _, ok := m.selected[m.cursor]
-            if ok {
-                delete(m.selected, m.cursor)
-            } else {
-                m.selected[m.cursor] = struct{}{}
-            }
+// The "enter" key and the spacebar (a literal space) toggle
+// the selected state for the item that the cursor is pointing at.
+case "enter", " ":
+_, ok := m.selected[m.cursor]
+if ok {
+    delete(m.selected, m.cursor)
+} else {
+    m.selected[m.cursor] = struct{}{}
+}
         }	
     }
 
-    // Return the updated model to the Bubble Tea runtime for processing.
-    // Note that we're not returning a command.
-    return m, nil
+// Return the updated model to the Bubble Tea runtime for processing.
+// Note that we're not returning a command.
+return m, nil
 }
 
 func (m model) View() string {
